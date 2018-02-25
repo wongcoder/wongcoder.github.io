2079fe764820b0dec31ce66e34dab0c42fef7c79
Maki 3 <f0wlf4ng@gmail.com>
2018-02-25 12:06:10 -0700

    Yeah,  I have no idea what I'm doing

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20180118213507) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.integer "idNum"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
